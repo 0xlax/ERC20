61e5be67d5afb3b5d5361651423f7a29d0089081
harshithlaxman <harshu8310@gmail.com>
2021-08-11 11:36:15 +0530

    xlean

--- contracts/ERC20.sol
+++ contracts/ERC20.sol
@@ -1,1 +1,3 @@
+from brownie import acocunts, config, Easytoken, EasyToken
 
+from scripts.helpful_scripts import get_accounts
--- contracts/token.sol
+++ contracts/token.sol
@@ -1,2 +1,8 @@
+pragma solidity ^0.6.0;
 
-
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol"
+contract Token is ERC20 {
+	constructror() public ERC20("Token", "TOK") {
+	_mint(msg.sender, 100000000000000000)
+	}
+}
